236add119c64066da89ff4f50f47b537a72fe1d2
nnadya <n.d.niki2012@gmail.com>
2024-12-21 01:08:34 +0300

    add types

--- my-app/src/utils/sortUsers.ts
+++ my-app/src/utils/sortUsers.ts
@@ -1,5 +1,7 @@
-const sortUsers = (users, sortConfig) =>
-  users.sort((a, b) => {
+import { User, SortConfig } from "../types/myTypes";
+
+const sortUsers = (users: User[], sortConfig: SortConfig): User[] =>
+  users.sort((a: User, b: User) => {
     if (sortConfig.field === null) return 0;
     if (a[sortConfig.field] < b[sortConfig.field]) {
       return sortConfig.direction === "ascending" ? -1 : 1;
